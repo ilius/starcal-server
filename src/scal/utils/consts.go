2413e39d1e083e888213ade543de0e88e418ea3d
Saeed Rasooli <saeed.gnu@gmail.com>
2016-08-31 19:20:39 +0430

    split utils.go into funcs.go and consts.go

---
+++ src/scal/utils/consts.go
@@ -0,0 +1,20 @@
+package utils
+
+var MIN_INT64 = int64(-9223372036854775808)
+var J1970 = 2440588
+
+var DurationUnitByValue = map[int]string{
+    1: "second",
+    60: "minute",
+    3600: "hour",
+    86400: "day",
+    604800: "week",
+}
+
+//DurationUnitList=[(1, "second"), (60, "minute"), (3600, "hour"), (86400, "day"), (604800, "week")]
+
+
+func init() {
+    //fmt.Printf("")
+    //fmt.Println("DurationUnitByValue =", DurationUnitByValue)
+}
--- src/scal/utils/funcs.go
+++ src/scal/utils/funcs.go
@@ -10,23 +10,9 @@ import "scal"
 import "scal/cal_types"
 import "scal/cal_types/gregorian"
 
-var MIN_INT64 = int64(-9223372036854775808)
-var J1970 = 2440588
-
-var DurationUnitByValue = map[int]string{
-    1: "second",
-    60: "minute",
-    3600: "hour",
-    86400: "day",
-    604800: "week",
-}
-
-//DurationUnitList=[(1, "second"), (60, "minute"), (3600, "hour"), (86400, "day"), (604800, "week")]
-
 
 func init() {
     fmt.Printf("")
-    //fmt.Println("DurationUnitByValue =", DurationUnitByValue)
 }
 
 func IntMin(a, b int) int {
