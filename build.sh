7fb09c074eb6238a8f1421a3f8f4d54c699d3e0c
Saeed Rasooli <saeed.gnu@gmail.com>
2016-09-08 03:19:56 +0430

    add submodules: gorilla/mux and gorilla/context

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 GOPATH=$PWD go install gopkg.in/mgo.v2-unstable
+GOPATH=$PWD go install github.com/gorilla/context
+GOPATH=$PWD go install github.com/gorilla/mux
 #GOPATH=$PWD go install scal
