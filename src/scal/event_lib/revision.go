a0db9a21b3d198d10656ac9294da45fa4d0fd5e3
Saeed Rasooli <saeed.gnu@gmail.com>
2016-09-11 23:56:17 +0430

    fix revision.go: add bson tags, change Hash to Sha1

--- src/scal/event_lib/revision.go
+++ src/scal/event_lib/revision.go
@@ -1,14 +1,15 @@
 package event_lib
 
 import "time"
+import "gopkg.in/mgo.v2-unstable/bson"
 
 type EventRevisionModel struct {
-    UserId int
-    EventId string
-    EventType string
-    Hash string
-    Time time.Time
-    //InvitedUserIds []int
+    UserId int              `bson:"userId"`
+    EventId bson.ObjectId   `bson:"eventId"`
+    EventType string        `bson:"eventType"`
+    Sha1 string             `bson:"sha1"`
+    Time time.Time          `bson:"time"`
+    //InvitedUserIds []int  `bson:"invitedUserIds"`
 }
 
 
