1db5ed9190936fbd250ec80f5a559dbf096e38a6
Saeed Rasooli <saeed.gnu@gmail.com>
2016-09-12 21:40:27 +0430

    add submodues: golang.org/x/crypto, golang.org/x/net

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 GOPATH=$PWD go install gopkg.in/mgo.v2-unstable
 GOPATH=$PWD go install github.com/gorilla/context
 GOPATH=$PWD go install github.com/gorilla/mux
+GOPATH=$PWD go install golang.org/x/crypto/bcrypt
+GOPATH=$PWD go install golang.org/x/net/context
 #GOPATH=$PWD go install scal
