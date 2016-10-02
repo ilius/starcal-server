0b3ea2e0afbb844c9b5a0cf012a358545d575e84
Saeed Rasooli <saeed.gnu@gmail.com>
2016-10-02 19:36:08 +0330

    cleaning: move some codes from user_handlers.go to user_model.go

--- src/scal/rest_server/user_handlers.go
+++ src/scal/rest_server/user_handlers.go
@@ -2,13 +2,10 @@ package rest_server
 
 import (
     "fmt"
-    "log"
     "net/http"
     "encoding/json"
     "io/ioutil"
     "crypto/md5"
-
-
 
     "gopkg.in/mgo.v2-unstable/bson"
     "gopkg.in/mgo.v2-unstable"
@@ -31,39 +28,6 @@ func init() {
 }
 
 
-type UserModel struct {
-    Id bson.ObjectId    `bson:"_id,omitempty" json:"-"`
-    Email string        `bson:"email" json:"email"`
-    FullName string     `bson:"fullName" json:"fullName"`
-    Password string     `bson:"password" json:"password"`
-    Locked bool         `bson:"locked" json:"-"`
-    DefaultGroupId *bson.ObjectId    `bson:"defaultGroupId" json:"defaultGroupId"`
-}
-
-func UserModelByEmail(email string, db *mgo.Database) *UserModel {
-    user := UserModel{}
-    err := db.C("users").Find(bson.M{
-        "email": email,
-    }).One(&user)
-    if err != nil {
-        if err != mgo.ErrNotFound {
-            log.Print("unkown error in fetching user model:", err)
-        }
-        return nil
-    }
-    return &user
-}
-
-func SetHttpErrorUserNotFound(w http.ResponseWriter, email string) {
-    SetHttpError(
-        w,
-        http.StatusInternalServerError,
-        fmt.Sprintf(
-            "user with email '%s' not found",
-            email,
-        ),
-    )
-}
 
 //type Request http.Request
 /*
---
+++ src/scal/rest_server/user_model.go
@@ -0,0 +1,45 @@
+package rest_server
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+
+    "gopkg.in/mgo.v2-unstable/bson"
+    "gopkg.in/mgo.v2-unstable"
+)
+
+type UserModel struct {
+    Id bson.ObjectId    `bson:"_id,omitempty" json:"-"`
+    Email string        `bson:"email" json:"email"`
+    FullName string     `bson:"fullName" json:"fullName"`
+    Password string     `bson:"password" json:"password"`
+    Locked bool         `bson:"locked" json:"-"`
+    DefaultGroupId *bson.ObjectId    `bson:"defaultGroupId" json:"defaultGroupId"`
+}
+
+func UserModelByEmail(email string, db *mgo.Database) *UserModel {
+    user := UserModel{}
+    err := db.C("users").Find(bson.M{
+        "email": email,
+    }).One(&user)
+    if err != nil {
+        if err != mgo.ErrNotFound {
+            log.Print("unkown error in fetching user model:", err)
+        }
+        return nil
+    }
+    return &user
+}
+
+func SetHttpErrorUserNotFound(w http.ResponseWriter, email string) {
+    SetHttpError(
+        w,
+        http.StatusInternalServerError,
+        fmt.Sprintf(
+            "user with email '%s' not found",
+            email,
+        ),
+    )
+}
+
