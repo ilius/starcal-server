829aceb40d911ae130538d9d2aa477cd9374e97b
Saeed Rasooli <saeed.gnu@gmail.com>
2016-10-06 20:39:25 +0330

    add client scripts for monthly event

---
+++ client/event/monthly/add.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+argv[1]: email
+argv[2]: password
+argv[3]: groupId, optional
+"""
+
+import sys
+import requests
+from requests.auth import HTTPDigestAuth
+from pprint import pprint
+from datetime import datetime, timedelta
+import time
+from time import strftime, gmtime 
+import random
+
+todayJd = datetime.now().toordinal() + 1721425
+dayStartSeconds = random.randint(0, 24*3600-1)
+
+params = {
+    #"eventId": "57d5e9fee576da5246cbe122",# must show: "you can't specify 'eventId'"
+    "timeZone": "Asia/Tehran",
+    "calType": "jalali",
+    "startJd": todayJd - 365,
+    "endJd": todayJd + 365,
+    "day": random.randint(1, 29),
+    "dayStartSeconds": dayStartSeconds,
+    "dayEndSeconds": dayStartSeconds + 3600,
+    "summary": "daily note 1",
+    "description": "desc 1",
+    "icon": "note.png",
+    "durationUnit": 0,
+}
+
+email, password = sys.argv[1:3]
+
+try:
+    params["groupId"] = sys.argv[3]
+except IndexError:
+    pass
+
+r = requests.post(
+    "http://127.0.0.1:8080/event/monthly/",
+    auth=HTTPDigestAuth(email, password),
+    json=params,
+)
+print(r)
+try:
+    data = r.json()
+except:
+    print("non-json data")
+    print(r.text)
+else:
+    error = data.get('error', '')
+    if error:
+        print(error)
+    else:
+        pprint(data, width=80)
