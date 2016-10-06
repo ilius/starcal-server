878e89feb3055dabe6c3ac43e4300811b9ccf7da
Saeed Rasooli <saeed.gnu@gmail.com>
2016-10-06 20:45:45 +0330

    add client scripts for weekly event

---
+++ client/event/weekly/get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+argv[1]: email
+argv[2]: password
+argv[3]: eventId
+"""
+
+import sys
+import requests
+from requests.auth import HTTPDigestAuth
+from pprint import pprint
+from datetime import datetime, timedelta
+
+email, password, eventId = sys.argv[1:4]
+
+r = requests.get(
+    "http://127.0.0.1:8080/event/weekly/%s/" % eventId,
+    auth=HTTPDigestAuth(email, password),
+)
+print(r)
+try:
+    data = r.json()
+except:
+    print("non-json data")
+    print(r.text)
+else:
+    error = data.get('error', '')
+    if error:
+        print(error)
+    else:
+        pprint(data, width=80)
+        print('Start Date:', datetime.fromordinal(data["startJd"] - 1721425))
+        print('End Date:', datetime.fromordinal(data["endJd"] - 1721425))
---
+++ client/event/weekly/update.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+argv[1]: email
+argv[2]: password
+argv[3]: eventId
+"""
+
+import sys
+import requests
+from requests.auth import HTTPDigestAuth
+from pprint import pprint
+from datetime import datetime, timedelta
+import time
+from time import strftime, gmtime
+import random
+
+email, password, eventId = sys.argv[1:4]
+
+todayJd = datetime.now().toordinal() + 1721425
+dayStartSeconds = random.randint(0, 24*3600-1)
+
+params = {
+    "timeZone": "Asia/Tehran",
+    "calType": "jalali",
+    "startJd": todayJd - 365,
+    "endJd": todayJd + 2*365,
+    "cycleWeeks": random.randint(1, 4),
+    "dayStartSeconds": dayStartSeconds,
+    "dayEndSeconds": dayStartSeconds + 3600,
+    "summary": "weekly event 1",
+    "description": "desc 1",
+    "icon": "",
+    "durationUnit": 0,
+}
+
+r = requests.put(
+    "http://127.0.0.1:8080/event/weekly/%s/" % eventId,
+    auth=HTTPDigestAuth(email, password),
+    json=params,
+)
+print(r)
+try:
+    data = r.json()
+except:
+    print("non-json data")
+    print(r.text)
+else:
+    error = data.get('error', '')
+    if error:
+        print(error)
+    else:
+        pprint(data, width=80)
