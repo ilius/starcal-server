b4e451356c873c3e19e65a2edd4180b781aee7ff
Saeed Rasooli <saeed.gnu@gmail.com>
2016-11-12 02:50:59 +0330

    deploy.sh: more safety and less downtime

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,18 @@ fi
 
 ./build.sh
 
+PROD_BIN=/usr/local/sbin/starcal-server
+
 echo "Compressing binary" ; bzip2 -kf server-$STARCAL_HOST || exit 1
-echo "Copying compressed binary to host" ; scp server-$STARCAL_HOST.bz2 root@$STARCAL_HOST:/tmp/starcal-server.bz2 || exit 1
-echo "Uncompressing binary" ; ssh root@$STARCAL_HOST "bunzip2 -f /tmp/starcal-server.bz2" || exit 1
+echo "Copying compressed binary to host" ; scp server-$STARCAL_HOST.bz2 root@$STARCAL_HOST:$PROD_BIN-new.bz2 || exit 1
+echo "Uncompressing binary" ; ssh root@$STARCAL_HOST "bunzip2 -f $PROD_BIN-new.bz2" || exit 1
 
 echo "Stopping service, updating binary and starting service again"
-ssh root@$STARCAL_HOST "service starcal stop
-cp /tmp/starcal-server /usr/local/sbin/starcal-server
+ssh root@$STARCAL_HOST "ls -l $PROD_BIN-new || exit 1
+service starcal stop
+mv -f $PROD_BIN-new $PROD_BIN
 service starcal start
-rm /tmp/starcal-server*"
+service starcal status"
 
 echo "Cleaning up" ; rm server-$STARCAL_HOST.bz2
 
