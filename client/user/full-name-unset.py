90daf5ba3be4a0b7c8076186d8b0f29ebe171ad0
Saeed Rasooli <saeed.gnu@gmail.com>
2017-09-24 09:54:04 +0800

    client: fix NameErrors in user/full-name-*.py

--- client/user/full-name-unset.py
+++ client/user/full-name-unset.py
@@ -9,6 +9,7 @@ import requests
 from pprint import pprint
 
 host = os.getenv("STARCAL_HOST", "127.0.0.1")
+token = os.getenv("STARCAL_TOKEN")
 if not token:
 	print("Please set and export STARCAL_TOKEN")
 	sys.exit(1)
