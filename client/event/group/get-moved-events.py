bb86779514ef17439edfdb6f8cc459efeb3d3fc3
Saeed Rasooli <saeed.gnu@gmail.com>
2018-12-25 12:16:09 +0800

    client

--- client/event/group/get-moved-events.py
+++ client/event/group/get-moved-events.py
@@ -9,6 +9,7 @@ import os
 import requests
 
 from pprint import pprint
+from dateutil.parser import parse as parseDatetime
 
 host = os.getenv("STARCAL_HOST", "127.0.0.1")
 token = os.getenv("STARCAL_TOKEN")
@@ -16,13 +17,15 @@ if not token:
 	print("Please set and export STARCAL_TOKEN")
 	sys.exit(1)
 
-groupId, sinceDateTime = sys.argv[1:3]
+groupId, sinceDateTimeInput = sys.argv[1:3]
+
+sinceDateTime = parseDatetime(sinceDateTimeInput)
 
 r = requests.get(
 	"http://%s:9001/event/groups/%s/moved-events/%s/" % (
 		host,
 		groupId,
-		sinceDateTime,
+		sinceDateTime.strftime("%Y-%m-%dT%H:%M:%SZ"),
 	),
 	headers={"Authorization": "bearer " + token},
 )
