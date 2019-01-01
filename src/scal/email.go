8dbffbf380b524cf6c69d8151115743952921c69
Saeed Rasooli <saeed.gnu@gmail.com>
2019-01-02 02:01:45 +0800

    scal/email.go: cleaning by adding in.GetContentType()

--- src/scal/email.go
+++ src/scal/email.go
@@ -12,21 +12,22 @@ type SendEmailInput struct {
 	Body    string
 }
 
+func (in *SendEmailInput) GetContentType() string {
+	if in.IsHtml {
+		return "text/html"
+	}
+	return "text/plain"
+}
+
 func SendEmail(in *SendEmailInput) error {
 	cmd := exec.Command(
 		"/usr/sbin/exim4",
 		"-i",
 		in.To,
 	)
-	var contentType string
-	if in.IsHtml {
-		contentType = "text/html"
-	} else {
-		contentType = "text/plain"
-	}
 	cmd.Stdin = strings.NewReader(
 		"From: StarCalendar <noreply@starcalendar.net>\n" +
-			"Content-Type: " + contentType + "\n" +
+			"Content-Type: " + in.GetContentType() + "\n" +
 			"To: " + in.To + "\n" +
 			"Subject: " + in.Subject + "\n" +
 			in.Body)
