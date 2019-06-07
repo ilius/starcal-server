617b2612cc547c4de37733dd27fb3e7eaf351a13
Saeed Rasooli <saeed.gnu@gmail.com>
2019-06-07 09:07:25 +0430

    build.sh: run git submodule update --init

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ if [ -z $STARCAL_HOST ] ; then
 	exit 1
 fi
 
+# takes ~ 0.2 seconds if submodules are already initialized / cloned
+git submodule update --init
+
 #GOPATH=$PWD go install github.com/globalsign/mgo
 #GOPATH=$PWD go install golang.org/x/crypto/bcrypt
 #GOPATH=$PWD go install golang.org/x/net/context
