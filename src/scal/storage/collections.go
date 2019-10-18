1f905477b3d8ac28fa0dd4c645fc65a35b07b915
Saeed Rasooli <saeed.gnu@gmail.com>
2019-10-19 02:44:52 +0330

    collections.go: organize collection names, add model struct names and Insert-only comments

--- src/scal/storage/collections.go
+++ src/scal/storage/collections.go
@@ -2,18 +2,21 @@ package storage
 
 // MongoDB Collection Names:
 const (
-	C_user               = "users"
-	C_userChangeLog      = "user_change_log"
-	C_userLogins         = "user_logins"
-	C_group              = "event_group"
-	C_eventMeta          = "event_meta"
-	C_attending          = "event_attending"
-	C_eventMetaChangeLog = "event_meta_change_log"
-	C_revision           = "event_revision"
-	C_eventData          = "event_data"
-	C_invitation         = "event_invitation"
-	C_resetPwToken       = "reset_password_token"
-	C_resetPwLog         = "reset_password_log"
+	C_user       = "users"       // UserModel
+	C_userLogins = "user_logins" // UserLoginAttemptModel
+	C_group      = "event_group" // EventGroupModel
+
+	C_eventMeta = "event_meta"     // EventMetaModel
+	C_revision  = "event_revision" // EventRevisionModel
+	C_eventData = "event_data"     // BaseEventModel
+
+	C_attending    = "event_attending"      // EventAttendingModel
+	C_resetPwToken = "reset_password_token" // ResetPasswordTokenModel
+
+	C_invitation         = "event_invitation"      // Insert-only, EventInvitationModel
+	C_userChangeLog      = "user_change_log"       // Insert-only, UserChangeLogModel
+	C_eventMetaChangeLog = "event_meta_change_log" // Insert-only, EventMetaChangeLogModel
+	C_resetPwLog         = "reset_password_log"    // Insert-only, ResetPasswordLogModel
 
 	C_errorsPrefix = "errors_"
 )
